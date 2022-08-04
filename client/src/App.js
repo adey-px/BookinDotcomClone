c9101ac32d8754bdfc6682ed051b16e072f0a91b
adey-px <aosteph@gmail.com>
2022-08-03 22:29:37 -0500

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,22 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Home from "./pages/home/Home";
+import Hotel from "./pages/hotel/Hotel";
+import Hotels from "./pages/list/Hotels";
+
+
+function App() {
+  return (
+
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/hotel/:id" element={<Hotel />} />
+        <Route path="/hotels" element={<Hotels />} />
+      </Routes>
+  </BrowserRouter>
+
+  );
+}
+
+export default App;
